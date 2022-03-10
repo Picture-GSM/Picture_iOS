2e8cc5a25655da70c066fc3373445807e46bff81
JiHoonAhn <ahnjh1028@naver.com>
2022-03-10 19:57:18 +0900

    [UPDATE] file setting

---
+++ Picture/AppRoot/AppRootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AppRootViewController.swift
+//  Picture
+//
+//  Created by Ji-hoon Ahn on 2022/03/10.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol AppRootPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class AppRootViewController: UIViewController, AppRootPresentable, AppRootViewControllable {
+
+    weak var listener: AppRootPresentableListener?
+}
