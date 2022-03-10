d42e49674500b11d423eb836b508d2889c3660d6
JiHoonAhn <ahnjh1028@naver.com>
2022-03-10 19:49:45 +0900

    [DELETE] SceneDelegate

---
+++ Picture/AppDelegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Picture
+//
+//  Created by Ji-hoon Ahn on 2022/03/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
